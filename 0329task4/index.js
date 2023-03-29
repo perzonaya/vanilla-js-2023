3bab0630404e130b9c3130563ffa6d07bdb902df
perzonaya <perzonaya@naver.com>
2023-03-29 14:18:01 +0900

    Update index.js

--- 0329task4/index.js
+++ 0329task4/index.js
@@ -11,8 +11,6 @@ function ddayClock(){
     const hours     = String(Math.floor((dday / (1000 * 60 * 60)) % 24)).padStart(2,"0");
     const minutes   = String(Math.floor((dday / (1000 * 60)) % 60)).padStart(2,"0");
     const seconds   = String(Math.floor((dday / 1000) % 60)).padStart(2,"0");
-
-
    
     clockTitle.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`
 }
