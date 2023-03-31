cb51b0222c0cd055643fdadbe90acf7c3a560dcc
perzonaya <perzonaya@naver.com>
2023-03-31 19:54:16 +0900

    finish class

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -1,39 +1,48 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
-const toDos =[];
-
-
-
-function saveToDos(){
-    localStorage.setItem("todos",toDos);
+const TODOS_KEY = "todos";
+let toDos = [];
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
-
 
 function deleteToDo(event) {
-    const li = event.target.parentElement;
-    li.remove();
-  }
-
-function paintToDo(newTodo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-     li.appendChild(span);
-     span.innerText = newTodo;
-    const button = document.createElement("button");
-     button.innerText = "☑";
-     button.addEventListener("click", deleteToDo);
-     li.appendChild(span);
-     li.appendChild(button);
-     toDoList.appendChild(li);
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
-
-function handleToDoSubmit(event) { 
-    event.preventDefault();
-   const newTodo =toDoInput.value;
-   toDoInput.value = "";
-   toDos.push(newTodo);
-   paintToDo(newTodo);
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),//각 아이템을 삭제하기 위해서 아이디를 주어준다    
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);//string을 array로 변환해줘
+  toDos=parsedToDos;
+parsedToDos.forEach(paintToDo);
+
+}
